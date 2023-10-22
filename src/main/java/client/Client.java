7829642bb410608427904cda4fe7530e9153ca3a
ischeglov <Ivan_mix@mail.ru>
2023-10-22 20:24:43 +0300

    add basic logic

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,2 +1,29 @@
-package client;public class Client {
+package client;
+
+import server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("netology.homework", Server.LOCALHOST_PORT);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            System.out.println(reader.readLine()); // читаем сообщение от сервера
+            writer.println("Ivan"); // клиент отправляет ответ
+
+            System.out.println(reader.readLine()); // читаем сообщение от сервера
+            writer.println("yes"); // клиент отправляет ответ
+            System.out.println(reader.readLine()); // читаем сообщение от сервера
+
+//            writer.println("no");
+//            System.out.println(reader.readLine());
+        }
+    }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,2 +1,41 @@
-package server;public class Server {
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static final Integer LOCALHOST_PORT = 8088;
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(LOCALHOST_PORT)) {
+            System.out.println("Сервер стартанул");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    outWriter.println("Write your name?"); // отправляем сообщение клиенту
+                    String name = inReader.readLine();
+                    System.out.println("Меня зовут : " + name); // читаем сообщение от клиента и выводим в консоль
+
+                    outWriter.println("Are you child? (yes/no)"); // отправляем сообщение клиенту
+                    String answer = inReader.readLine();
+                    System.out.println(name + " ответил " + answer); // читаем сообщение от клиента и выводим в консоль
+
+                    if (answer.equals("yes")) {
+                        outWriter.println("Welcome to the kids area, " + name + " Let's play!");
+                    } else {
+                        outWriter.println("Welcome to the adult zone, " +  name + " Have a good rest, or a good working day!");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
